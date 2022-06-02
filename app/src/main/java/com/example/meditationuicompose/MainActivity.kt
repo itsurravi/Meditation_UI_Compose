211e363ecb7961b08b4c3685ac485da5ae36829d
Ravikant Sharma <ravikant.sharma@prepladder.com>
2022-06-02 13:06:37 +0530

    Initial commit

---
+++ app/src/main/java/com/example/meditationuicompose/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.meditationuicompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.meditationuicompose.ui.HomeScreen
+import com.example.meditationuicompose.ui.theme.MeditationUIComposeTheme
+
+@ExperimentalFoundationApi
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MeditationUIComposeTheme {
+                HomeScreen()
+            }
+        }
+    }
+}
